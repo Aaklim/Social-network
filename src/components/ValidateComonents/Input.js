dee416e690e738d171ad85615bcb4fe3d33f6549
AndreiKlimovich <andrei.klimovich84@gmail.com>
2020-11-10 22:56:36 +0300

    input length to 140

--- src/components/ValidateComonents/Input.js
+++ src/components/ValidateComonents/Input.js
@@ -4,11 +4,11 @@ import cn from 'classnames';
 
 export const Input = ({ input, meta, ...props }) => {
   let isError =
-    (input.value.length === 0 && meta.touched) || input.value.length > 49;
+    (input.value.length === 0 && meta.touched) || input.value.length > 139;
   return (
     <div className={cn({ [styles.error]: isError }, styles.wrapper)}>
       {isError ? <div className={styles.error}>{meta.error}</div> : null}
-      <input {...input} {...props} id='input' maxLength='50' />
+      <input {...input} {...props} id='input' maxLength='140' />
     </div>
   );
 };
